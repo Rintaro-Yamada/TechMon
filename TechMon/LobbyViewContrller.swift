38c9b334eb8d1ffb45970f42692e3c604a489816
Rintaro Yamada <RintaroRobert14sei@gmail.com>
2021-05-15 20:43:06 +0900

    TechMonの完成

---
+++ TechMon/LobbyViewContrller.swift
@@ -0,0 +1,70 @@
+//
+//  LobbyViewContrller.swift
+//  TechMon
+//
+//  Created by 山田倫太郎 on 2021/05/15.
+//
+
+import UIKit
+
+class LobbyViewContrller: UIViewController {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var staminaLabel: UILabel!
+    
+    let techMonManager = TechMonManager.shared
+    
+    var stamina: Int = 100
+    var staminaTimer: Timer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        nameLabel.text = "勇者"
+        staminaLabel.text = "\(stamina) / 100"
+        staminaTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(updateStaminaValue), userInfo: nil, repeats: true)
+        staminaTimer.fire()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        techMonManager.stopBGM()
+    }
+    
+    @IBAction func toBattle() {
+        if stamina >= 50 {
+            stamina -= 50
+            staminaLabel.text = "\(stamina) / 100"
+            performSegue(withIdentifier: "toBattle", sender: nil)
+        } else {
+            let alert = UIAlertController(
+                title: "バトルにいけません",
+                message: "スタミナをためてください",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @objc func updateStaminaValue() {
+        if stamina < 100 {
+            stamina += 1
+            staminaLabel.text = "\(stamina)/100"
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
